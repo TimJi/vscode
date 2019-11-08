670e1a6b207ef6e07fb57932b526ebddaa7c6d2d
Tim <timji0210@gmail.com>
2019-11-08 15:23:52 +0800

    modify eslintrc for testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,5 +27,23 @@ module.exports = {
     "react/display-name": 0,
     "react-hooks/exhaustive-deps": "warn", // 檢查 effect 的相依性
     "prettier/prettier": "error"
-  }
+  },
+  "overrides": [
+    {
+      "files": [
+        "**/*.test.js"
+      ],
+      "env": {
+        "jest": true // now **/*.test.js files' env has both es6 *and* jest
+      },
+      "plugins": ["jest"],
+      "rules": {
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error"
+      }
+    }
+  ],
 }
